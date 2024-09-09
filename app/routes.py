9ed842023fa6aad52cf6f5b89e8b8e795c621df2
eptle <btlk04@gmail.com>
2024-09-09 22:45:22 +0200

    create simple page

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from flask import redirect, render_template, flash, url_for
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    title = 'Main'
+    username = 'Eptel'
+    return render_template('index.html', title=title, username=username)
---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    SECRET_KEY = 'very_strong_password'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
