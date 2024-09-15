6f62c9f98adda688a4fabe81f22debe0f992c81d
eptle <btlk04@gmail.com>
2024-09-15 15:33:47 +0200

    drag and drop bug fix

--- app/static/js/drag_and_drop_tasks.js
+++ app/static/js/drag_and_drop_tasks.js
@@ -1,5 +1,22 @@
 let taskList = document.getElementById('task-list');
 new Sortable(taskList, {
     animation: 150,
-    ghostClass: 'blue-background-class'
+    onEnd: function (evt) {
+        let items = document.querySelectorAll('#task-list li');
+        let positions = [];
+        items.forEach((item, index) => {
+            positions.push({ id: item.dataset.taskId, position: index });
+        });
+
+        // Отправка новых позиций на сервер через AJAX
+        fetch('/update-task-positions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(positions),
+        }).then(response => response.json()).then(data => {
+            console.log('Updated successfully', data);
+        });
+    },
 });
